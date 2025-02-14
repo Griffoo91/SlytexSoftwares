4b675b4bd97c02ccd6d92f39e4fcbd79bd42dd05
@griffoo91 <omboggorised91@gmail.com>
2025-02-14 14:27:09 +0300

    background image width to fit and not extend the width of the screen

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
 
-    <div className="min-h-screen w-full bg-cover bg-center bg-fixed flex flex-col" 
+    <div className="min-h-screen w-full max-w-full bg-cover bg-center bg-no-repeat bg-fixed flex flex-col overflow-hidden" 
          style={{ backgroundImage: "url('/bg2.jpg')" }}>
             <div className="flex-1">{children}</div>
     </div>
