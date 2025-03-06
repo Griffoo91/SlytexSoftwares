8c48a67fc353194ad5105e9332d85e11aa385c6b
@griffoo91 <omboggorised91@gmail.com>
2025-03-06 18:25:46 +0300

    server.js update

--- backend/api/server.js
+++ backend/api/server.js
@@ -59,5 +59,8 @@ app.use((err, req, res, next) => {
 
 // Start Server
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 export default app;
