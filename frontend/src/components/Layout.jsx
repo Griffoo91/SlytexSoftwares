d4740d4bcbf917c61dc01fdcf243b20469150ee2
@griffoo91 <omboggorised91@gmail.com>
2025-03-25 15:41:50 +0300

    modified the bb image to bg2.webp

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,8 +3,7 @@ import React from "react";
 const Layout = ({ children }) => {
   return (
 
-    <div className="min-h-screen w-full max-w-full bg-cover bg-center bg-no-repeat bg-fixed flex flex-col overflow-hidden" 
-         style={{ backgroundImage: "url('/bg2.jpg')" }}>
+    <div className="min-h-screen w-full max-w-full bg-cover bg-center bg-no-repeat bg-fixed flex flex-col overflow-hidden bg-[url('/bg2.webp')]">
             <div className="flex-1">{children}</div>
     </div>
   );
